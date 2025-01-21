7a425516b6d351e99f8168f06f23cdc26a4e3d68
Tirtha Chakrabarti <chakrabartitirtha2@gmail.com>
2025-01-21 14:47:27 +0530

    feat:Project-one updated

---
+++ Project-one/test-project-one/my-app/src/app/page.jsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <div>Home!</div>
+      <div>
+        <p>Welcome to the home page!</p>
+      </div>
+    </>
+  );
+}
