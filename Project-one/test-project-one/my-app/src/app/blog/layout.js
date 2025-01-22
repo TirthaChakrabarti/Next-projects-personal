6bcd61f46bb20c600ffc6ce158ec2aa307839763
Tirtha Chakrabarti <chakrabartitirtha2@gmail.com>
2025-01-22 16:03:55 +0530

    feat:new codes added to Project-one

---
+++ Project-one/test-project-one/my-app/src/app/blog/layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const layout = ({ children }) => {
+  return (
+    <div>
+      blog layout
+      {children}
+    </div>
+  );
+};
+
+export default layout;
---
+++ Project-one/test-project-one/my-app/src/app/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <div>All Rights Reserved</div>
+      <div>Social Links</div>
+    </div>
+  );
+};
+
+export default Footer;
