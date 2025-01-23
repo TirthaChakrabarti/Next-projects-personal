6c5d0b2c73e562253e113005b0d89151d4311afe
Tirtha Chakrabarti <chakrabartitirtha2@gmail.com>
2025-01-23 12:51:04 +0530

    feat: codes added

--- Project-one/test-project-one/my-app/src/app/components/navbar/Navbar.jsx
+++ Project-one/test-project-one/my-app/src/app/components/navbar/Navbar.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
 
@@ -36,14 +38,19 @@ const links = [
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="container">
       {links.map((link) => (
         <Link key={link.id} href={link.url}>
           {link.title}
         </Link>
       ))}
+      <button onClick={() => console.log("Logged in")}>Login</button>
+      <button onClick={() => console.log("Logged out")}>Logout</button>
     </div>
   );
 };
+
+// Above onClick events (or other such events) need this Navbar component to be client-side-rendered
+// We have added 'use client' at the top to ensure this
 
 export default Navbar;
