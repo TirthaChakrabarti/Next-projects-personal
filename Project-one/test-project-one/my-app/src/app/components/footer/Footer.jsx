94472b55c621461521bb172a5941e554a6f98edc
Tirtha Chakrabarti <chakrabartitirtha2@gmail.com>
2025-02-04 14:29:44 +0530

    feat: image related codes added

--- Project-one/test-project-one/my-app/src/app/components/footer/Footer.jsx
+++ Project-one/test-project-one/my-app/src/app/components/footer/Footer.jsx
@@ -5,13 +5,37 @@ const Footer = () => {
   return (
     <div className={styles.container}>
       <div>All Rights Reserved</div>
-      <div>
+      <div className={styles.logos}>
         {/* <Image
-          src="./footer-logo/fb-logo.webp"
+          src="./footer-logos/fb-logo.webp"
           width={15}
           height={15}
           alt="MyApp"
         /> */}
+        <img
+          src="./footer-logos/fb-logo.webp"
+          alt="Facebook Logo"
+          width={30}
+          height={30}
+        />
+        <img
+          src="./footer-logos/x-logo.webp"
+          alt="X Logo"
+          width={30}
+          height={30}
+        />
+        <img
+          src="./footer-logos/youtube.png"
+          alt="Youtube Logo"
+          width={30}
+          height={30}
+        />
+        <img
+          src="./footer-logos/whatsapp.png"
+          alt="Whatsapp Logo"
+          width={30}
+          height={30}
+        />
       </div>
     </div>
   );
