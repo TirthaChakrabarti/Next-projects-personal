a9c99f66d8e83fca4a1324fc556ecce17a76d182
Tirtha Chakrabarti <chakrabartitirtha2@gmail.com>
2025-04-07 19:25:52 +0530

    feat: new codes and notes

---
+++ Codevolution-Next/my-app/src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div>
+      <h2>Function not found</h2>
+      <p>Could not find requested resource</p>
+    </div>
+  );
+}
---
+++ Codevolution-Next/my-app/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -0,0 +1,13 @@
+export default async function ProductReview({
+  params,
+}: {
+  params: Promise<{ productId: string; reviewId: string }>;
+}) {
+  const { productId, reviewId } = await params;
+  return (
+    <div>
+      <h1>Product {productId}</h1>
+      <h2>Review {reviewId}</h2>
+    </div>
+  );
+}
