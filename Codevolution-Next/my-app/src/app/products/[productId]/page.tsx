e98e598a82313173ae82d251ea503d813717651b
Tirtha Chakrabarti <chakrabartitirtha2@gmail.com>
2025-04-10 19:05:28 +0530

    feat: new codes and notes

--- Codevolution-Next/my-app/src/app/products/[productId]/page.tsx
+++ Codevolution-Next/my-app/src/app/products/[productId]/page.tsx
@@ -1,14 +1,29 @@
-export default async function productDetails({
+import { Metadata } from "next";
+
+type Props = {
+  params: Promise<{ productId: string }>;
+};
+
+export const generateMetadata = async ({
   params,
-}: {
-  params: Promise<{
-    productId: string;
-  }>;
-}) {
+}: Props): Promise<Metadata> => {
+  const id = (await params).productId;
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`iPhone ${id}`);
+    }, 100);
+  });
+  return {
+    title: `Product ${title}`,
+    description: `This is the page for product ${id}`,
+  };
+};
+
+export default async function productDetails({ params }: Props) {
   const productId = (await params).productId;
   return (
     <div>
-      <h1>Product {productId}</h1>
+      <h1>Details about Product {productId}</h1>
     </div>
   );
 }
---
+++ Codevolution-Next/my-app/src/app/products/page.tsx
@@ -0,0 +1,11 @@
+export default function Products() {
+  return (
+    <div>
+      <h1>Products</h1>
+
+      <h2>Featured Products</h2>
+      <h2>Best Sellers</h2>
+      <h2>New Arrivals</h2>
+    </div>
+  );
+}
